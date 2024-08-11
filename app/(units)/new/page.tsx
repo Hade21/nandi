79526f6b76b408aad2c1562f9a90b48654b6a4b2
Hade21 <abdurrohmanmuhammad123@gmail.com>
2024-08-11 15:42:40 +0800

    feat: create new page to add new unit

---
+++ app/(units)/new/page.tsx
@@ -0,0 +1,11 @@
+import FormUnit from "@/components/FormUnit";
+
+const NewUnits = () => {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <FormUnit />
+    </div>
+  );
+};
+
+export default NewUnits;
