3113ac31296b12458e82666149263c0b686e8042
Hade21 <abdurrohmanmuhammad123@gmail.com>
2024-08-14 16:39:25 +0800

    feat: add new page to update units

--- app/(units)/update/[id]/page.tsx
+++ app/(units)/update/[id]/page.tsx
@@ -2,16 +2,15 @@ import FormUnit from "@/components/FormUnit";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "New Unit",
-  description: "Add new unit",
+  title: "Update Unit",
 };
 
-const NewUnits = () => {
+const Update = ({ params }: { params: { id: string } }) => {
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      <FormUnit type="new" />
+      <FormUnit type="update" id={params.id} />
     </div>
   );
 };
 
-export default NewUnits;
+export default Update;
