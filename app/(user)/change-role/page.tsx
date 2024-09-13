62fa2e55924cc01d36646335b85ddb82afe9321d
Hade21 <abdurrohmanmuhammad123@gmail.com>
2024-09-14 07:27:37 +0800

    feat: create change role page

---
+++ app/(user)/change-role/page.tsx
@@ -0,0 +1,11 @@
+import DataTable from "./data-table";
+
+const ChangeRole = async () => {
+  return (
+    <div className="w-full px-4 py-6">
+      <DataTable />
+    </div>
+  );
+};
+
+export default ChangeRole;
